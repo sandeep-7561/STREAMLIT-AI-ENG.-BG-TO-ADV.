395722f885412195543daede486600779b879472
Sandeep <prajapatisandeep635217@gmail.com>
2026-08-13 20:40:38 +0530

    add introduction

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ from streamlit_option_menu import option_menu
 
 st.set_page_config(page_title='AI Engineer',page_icon='items\logo.png',layout='wide')
 
+with st.sidebar:
+    select1 = option_menu('Admin',['Home'])
 
-if 'Introduction' not in st.session_state:
-    st.session_state.Introduction = False
+with st.sidebar:
+    select2 = option_menu('Title',['Introduction'])
 
-st.sidebar.title('TOPICS')
-Introduction = st.sidebar.button('Introduction')
-col1,col2,col3 = st.columns([3.5,3,3.5])
-with col2:
-    st.title("AI Engineer")
 
-if st.button('Introduction'):
-    st.session_state.Introduction = True
 
-if st.session_state.Introduction == True:
-    st.write('s')
+if select1 == 'Home':
+    col1,col2,col3 = st.columns([3.5,3,3.5])
+    with col2:
+        st.title("AI Engineer")
+    st.divider()
+    st.header('Introduction')
+    st.write('''
+        AI Engineer — From Beginner to Advanced,\n
+        This project is a complete, structured, and practical learning platform for aspiring AI Engineers, designed to take you from absolute beginner to advanced industry-ready level.\n
+        It covers the entire AI Engineering roadmap step by step, with every major concept organized line by line and layer by layer, so learners can build their knowledge progressively rather than jumping randomly between technologies.\n
+        From Python, Mathematics, Data Structures, Machine Learning, Deep Learning, NLP, Computer Vision, Generative AI, LLMs, Transformers, RAG, AI Agents, Vector Databases, MLOps, APIs, Cloud Deployment, and Production AI Systems — the roadmap is designed to cover the complete journey of an AI Engineer.\n
+        ''')
